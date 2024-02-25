693d9aa168e83f122989d1ecaeeeb77544ce6e6c
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-02-25 17:02:28 -0300

    strings

--- parte_I/cap_02/name.py
+++ parte_I/cap_02/name.py
@@ -3,3 +3,10 @@ print(f'name: {name}')
 print(f'title: {name.title()}')
 print(f'upper: {name.upper()}')
 print(f'lower: {name.lower()}')
+
+# Concatenação de Strings
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(f'full name (Tittle): {full_name.title()}')
