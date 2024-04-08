27cd107bd357c9b6cd754fc8672aab86bd861ef5
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-04-08 17:26:45 -0300

    models

--- parte_III/learning_log/learning_logs/models.py
+++ parte_III/learning_log/learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    """Assunto de estudo do usuário"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devolve uma representação em string do modelo"""
+        return self.text
+
+    
