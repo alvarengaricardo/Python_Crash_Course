f3281f1715811602e50fec17d0227181b1935a24
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-04-13 23:29:44 -0300

    Migração de Django 1.0 para 2.0 e correção de Bugs; Conclusão da consulta de tópicos; Cap. 18.

--- parte_III/learning_log/learning_log/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    """Página inicial"""
-    return render(request, 'learning_logs/index.html')
--- parte_III/learning_log/learning_logs/models.py
+++ parte_III/learning_log/learning_logs/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
+
 class Topic(models.Model):
-    """Disciplina de estudo do usuário - ex: Álgebra Linear"""
+    """A topic the user is learning about."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """Devolve uma representação em string do modelo"""
+        """Return a string representation of the model."""
         return self.text
 
+
 class Entry(models.Model):
-    """Especificação de um assunto - ex: Operações com Matrizes"""
+    """Something specific learned about a topic."""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
@@ -19,7 +21,5 @@ class Entry(models.Model):
         verbose_name_plural = 'entries'
 
     def __str__(self):
-        """Devolve uma representação em string do modelo, limitado a 50 caracteres"""
+        """Return a string representation of the model."""
         return self.text[:50] + "..."
-
-
